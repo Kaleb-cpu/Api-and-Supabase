eb4ad8125edc291685b47c12c40513d8f7e11cc6
Kaleb-cpu <kakuj424@gmail.com>
2023-03-28 14:40:19 -0600

    error

--- src/routes/achievements/a4-supabaseSetup/+page.server.js
+++ src/routes/achievements/a4-supabaseSetup/+page.server.js
@@ -3,6 +3,6 @@ import { supabase } from '$lib/supabaseClient';
 export async function load() {
 	const { data } = await supabase.from('countries').select();
 	return {
-		countries: data ?? []
+		countries: data ?? ['error']
 	};
 }

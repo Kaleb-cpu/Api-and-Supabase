95b64e286e636ef8f292a6ceca0583637ab3596f
Kaleb-cpu <kakuj424@gmail.com>
2023-03-29 23:35:16 -0600

    a5 added

---
+++ src/routes/achievements/a5-office/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data;
+  console.log(data)
+</script>
+
+<h1>Office Employees</h1>
+<ul>
+  {#each data.office_employee as employee}
+  <li>{employee.id}:{employee.name}</li>
+  {/each}
+</ul>
